0e831131d0c28e6e4ecd4bd9e76e495e90166b90
iguider <iguyder@gmail.com>
2018-03-11 12:02:37 +0000

    Correct a silly mistake

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once "bootstrap.php";
 
 echo Request::factory()
         ->execute()
-        ->headers("Access-Control-Allow-Origin", "*");
+        ->headers("Access-Control-Allow-Origin", "*")
         ->cors()
         ->send_headers(true)
         ->body();
